e59c7952d8b0d468056870097ed5687652be16ed
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-09-08 14:20:45 +0530

    header style almost completd

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-
-
-function Header(){
+function Header() {
     return (
-        <header className="{Styles.Header}">
-        <div className="{styles.container}">
-          <a id="logo" href="#">
-            Cartify
-          </a>
-          <nav>
-            <ul>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Products</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          <div className="actionLinks">
-            <a href="#">
-              <span className="material-symbols-outlined">favorite</span>
-            </a>
-            <a href="#">
-              <span className="material-symbols-outlined">local_mall</span>
-            </a>
-          </div>
-        </div>
-      </header>
-    )
+        <header className={styles.Header}>
+            <div className={styles.container}>
+                <a className={styles.logo} id="logo" href="#">
+                    Cartify
+                </a>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Products</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={styles.actionLinks}>
+                    <a href="#">
+                        <span className="material-symbols-outlined">favorite</span>
+                    </a>
+                    <a href="#">
+                        <span className="material-symbols-outlined">local_mall</span>
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
 }
 
-export default Header ;
+export default Header;
